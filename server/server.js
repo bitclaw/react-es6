2e2a58bc594958be7ad19789b04b600891c1011d
Daniel C <bitclaw@gmail.com>
2016-07-10 19:21:00 -0500

    Add index.html associated to app.js file.

--- server/server.js
+++ server/server.js
@@ -5,5 +5,9 @@ let app = express();
 
 app.server = http.createServer(app);
 app.get('/',(req,res) => res.send('Hello express!'));
+// Assets
+app.use(express.static('public'));
+
+// Listen
 app.server.listen(process.env.PORT || 3000);
 console.log('Server running at http://127.0.0.1:3000/');
